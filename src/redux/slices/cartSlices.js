389377c90f08ba3a4cde5b70f0a8c25228492777
Derek Engles <derekengles@gmail.com>
2025-05-29 11:37:54 -0400

    Connected Redux to React via Provider

---
+++ src/redux/slices/cartSlices.js
@@ -0,0 +1,63 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  items: [],
+  totalQuantity: 0,
+  totalCost: 0
+};
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    addItem(state, action) {
+      const newItem = action.payload;
+      const existingItem = state.items.find(item => item.id === newItem.id);
+
+      if (!existingItem) {
+        state.items.push({ ...newItem, quantity: 1 });
+      } else {
+        existingItem.quantity++;
+      }
+
+      state.totalQuantity++;
+      state.totalCost += newItem.price;
+    },
+
+    incrementItem(state, action) {
+      const id = action.payload;
+      const item = state.items.find(item => item.id === id);
+      if (item) {
+        item.quantity++;
+        state.totalQuantity++;
+        state.totalCost += item.price;
+      }
+    },
+
+    decrementItem(state, action) {
+      const id = action.payload;
+      const item = state.items.find(item => item.id === id);
+      if (item && item.quantity > 0) {
+        item.quantity--;
+        state.totalQuantity--;
+        state.totalCost -= item.price;
+        if (item.quantity === 0) {
+          state.items = state.items.filter(i => i.id !== id);
+        }
+      }
+    },
+
+    deleteItem(state, action) {
+      const id = action.payload;
+      const item = state.items.find(item => item.id === id);
+      if (item) {
+        state.totalQuantity -= item.quantity;
+        state.totalCost -= item.price * item.quantity;
+        state.items = state.items.filter(i => i.id !== id);
+      }
+    }
+  }
+});
+
+export const { addItem, incrementItem, decrementItem, deleteItem } = cartSlice.actions;
+export default cartSlice.reducer;
